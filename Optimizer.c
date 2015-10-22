d0baa2d46025d84b78bc77ef822a62343c806551
vinceix <vincentxie12@gmail.com>
2015-10-22 02:00:08 -0400

    Optimizer v1

--- Optimizer.c
+++ Optimizer.c
@@ -13,6 +13,40 @@
 #include "InstrUtils.h"
 #include "Utils.h"
 
+void markVariable(int reg, int offset, Instruction *ins){
+	Instruction *temp = ins;
+	while(temp != NULL){
+		if((temp->opcode == ADD || temp->opcode == SUB || temp->opcode == MUL || temp->opcode == DIV) && temp->field3 == reg){
+			temp->critical = 1;
+			markCode(temp->field1, temp);
+			markCode(temp->field2, temp);
+		} else if(temp->opcode == LOADAI && temp->field3 == reg){
+			temp->critical = 1;
+			markVariable(temp->field1, temp->field2, temp);
+		} else if(temp->opcode == LOADI && temp->field2 == reg){
+			temp->critical = 1;
+		}
+		temp = temp->prev;
+	}	
+}
+
+void markCode(int reg, Instruction *ins){
+	Instruction *temp = ins;
+	while(temp != NULL){
+		if((temp->opcode == ADD || temp->opcode == SUB || temp->opcode == MUL || temp->opcode == DIV) && temp->field3 == reg){
+			temp->critical = 1;
+			markCode(temp->field1, temp);
+			markCode(temp->field2, temp);
+		} else if(temp->opcode == LOADAI && temp->field3 == reg){
+			temp->critical = 1;
+			//markVariable(temp->field1, temp->field2, temp);
+		} else if(temp->opcode == LOADI && temp->field2 == reg){
+			temp->critical = 1;
+		}
+		temp = temp->prev;
+	}
+}
+
 int main()
 {
 	Instruction *head;
@@ -23,10 +57,37 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
-	/* YOUR CODE GOES HERE */
+	Instruction *temp = head;
+	while(temp != NULL){
+		temp->critical = 0;
+		temp = temp->next;
+	}
+	temp = head;
+	temp->critical = 1;
+	while(temp != NULL){
+		if(temp->opcode == OUTPUTAI){
+			temp->critical = 1;
+			Instruction *iterator = temp;
+			while(iterator != head){
+				if(iterator->opcode == STOREAI && iterator->field3 == temp->field2){
+					iterator->critical = 1;
+					markCode(iterator->field1, iterator);
+				}
+				iterator = iterator->prev;	
+			}
+		}
+		temp = temp->next;
+	}
 
 	if (head) 
 		PrintInstructionList(stdout, head);
+
+	temp = head;	
+	while(temp != NULL){
+		free(head);
+		temp = temp->next;
+		head = temp;	
+	}
 	
 	return EXIT_SUCCESS;
 }

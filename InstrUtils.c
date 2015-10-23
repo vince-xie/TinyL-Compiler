d5d9c927ea70bae50146aa871b32d9e8a0197e2f
vinceix <vincentxie12@gmail.com>
2015-10-23 17:05:42 -0400

    Final commit

--- InstrUtils.c
+++ InstrUtils.c
@@ -69,9 +69,13 @@ void PrintInstructionList(FILE * outfile, Instruction * instr)
 		ERROR("No instructions\n");
 		exit(EXIT_FAILURE);
 	}
-
-	/* YOUR CODE GOES HERE */
-
+	Instruction *temp = instr;
+	while(temp != NULL){
+		if(temp->critical == 1){
+			PrintInstruction(outfile, temp);
+		}
+		temp = temp->next;
+	}
 }
 
 Instruction *ReadInstruction(FILE * infile)
